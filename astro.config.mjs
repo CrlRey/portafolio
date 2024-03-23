4b421fcdfe76f92cf5005f7cf0e9acd76ae9492f
Carlos Rey <cruzreyesce@gmail.com>
2024-03-22 18:55:21 -0600

    Version 1.2.0

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,11 @@
 import { defineConfig } from 'astro/config';
-import alpinejs from "@astrojs/alpinejs";
-
 import icon from "astro-icon";
+
+import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [alpinejs(), icon({
-    iconDir: "src/icon",
-
-  })]
+  integrations: [icon({
+    iconDir: "src/icon"
+  }), react()]
 });
