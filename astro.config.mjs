e5b0617473ddb714cfb1f8b5c8e678025c0849ab
Carlos Rey <cruzreyesce@gmail.com>
2024-10-15 23:19:50 -0600

    add new job and fix card swiper

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,12 @@
 import { defineConfig } from 'astro/config';
 import icon from "astro-icon";
-
 import react from "@astrojs/react";
+
+import vue from "@astrojs/vue";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [icon({
     iconDir: "src/icon"
-  }), react()]
+  }), vue()]
 });
---
+++ src/helpers/experience.js
@@ -0,0 +1,27 @@
+
+
+export const data = [
+    {   
+        logo: 'https://i.postimg.cc/PxDMmjbL/img-footer.png',
+        title: 'ingeniero de soporte técnico',
+        year: '2022 - Julio 2023',
+        span1: 'Soporte Técnico y Resolución de Problemas:',
+        span2: 'Instalación de equipo de redes y CCTV:',
+        span3: 'Administración de cuentas con Office 365:',
+        task1: 'Proporcioné asistencia técnica a usuarios/as, diagnosticando y resolviendo problemas técnicos de manera eficiente para asegurar la continuidad operativa.',
+        task2: 'Planificar e implementar la instalación de equipos de redes y sistemas de videovigilancia (CCTV), asegurando una conectividad óptima y seguridad en todas las áreas cubiertas.',
+        task3: 'Configuración y administración de servicios de correo, almacenamiento y colaboración, y aplicando medidas de seguridad para proteger los datos y optimizar el uso de las herramientas.',
+    },
+
+    {
+        logo: 'https://framework-gb.cdn.gob.mx/gobmx/img/logo_blanco.svg',
+        title: 'Especialista en Optimización Administrativa y Tecnológica',
+        year: '2023 - Agosto 2024',
+        span1: 'Desarrollo de Aplicaciones Web:',
+        span2: 'Automatización y Herramientas Personalizadas:',
+        span3: 'Capacitación y Soporte:',
+        task1: 'Creación de soluciones personalizadas en Laravel y Vuejs paraautomatizar y mejorar procesos internos, optimizando la eficiencia operativa.',
+        task2: 'Manejo de herramientas en Excel y PostgreSQL para facilitar la gestión de datos y mejorar la toma de decisiones.',
+        task3: 'Formación de compañeros en el uso de nuevas herramientas y aplicaciones, garantizando una transición efectiva y un uso óptimo de los procesos mejorados.'
+    }
+] 
