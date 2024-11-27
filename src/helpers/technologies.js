3876b8df55f068fc7c785579562b7eaa0b263e13
Carlos Rey <cruzreyesce@gmail.com>
2024-11-26 21:02:12 -0600

    add mew project

---
+++ src/helpers/technologies.js
@@ -0,0 +1,64 @@
+export const tec = [
+    {
+        title: 'HTML',
+        subtext: 'lenguaje de etiquetas de hipertexto',
+        icon: 'https://res.cloudinary.com/dcmcgucbu/image/upload/f_auto,q_auto/v1/icon/ionhexp8lspqqufimaqs'
+    },
+
+    {
+        title: "CSS",
+        subtext: "Hojas de estilo en cascada",
+        icon: "https://res.cloudinary.com/dcmcgucbu/image/upload/f_auto,q_auto/v1/icon/z8ktrtbxrekarjaib6az"
+    },
+    {
+        title: "JavaScript",
+        subtext: "Lenguaje de programación compilado",
+        icon: "https://res.cloudinary.com/dcmcgucbu/image/upload/f_auto,q_auto/v1/icon/dl7spz9nqrg1ydciaqoy"
+    },
+    {
+        title: "Vue",
+        subtext: "Framework progresivo JavaScript",
+        icon: "https://res.cloudinary.com/dcmcgucbu/image/upload/f_auto,q_auto/v1/icon/djiousisl7rh05kdkq0r"
+    },
+    {
+        title: "Astro",
+        subtext: "Creador de sitios web estáticos y dinámicos",
+        icon: "https://res.cloudinary.com/dcmcgucbu/image/upload/f_auto,q_auto/v1/icon/jpjhl63dzstgmbxl7na1"
+    },
+    {
+        title: "PostgreSQL",
+        subtext: "Base de datos relacional",
+        icon: "https://res.cloudinary.com/dcmcgucbu/image/upload/f_auto,q_auto/v1/icon/he4hk9jqkpwcdjufx04n"
+    },
+    {
+        title: "Tailwind",
+        subtext: "Framework CSS utilitario para interfaces web",
+        icon: "https://res.cloudinary.com/dcmcgucbu/image/upload/f_auto,q_auto/v1/icon/hnkwhzi56frjlyclfayg"
+    },
+    {
+        title: "Python",
+        subtext: "Lenguaje de programación versátil",
+        icon: "https://res.cloudinary.com/dcmcgucbu/image/upload/f_auto,q_auto/v1/icon/smjuwpd5p36tphlsh25v"
+    },
+    {
+        title: "GitHub/Git",
+        subtext: "Plataforma para alojar y revisar código",
+        icon: "https://res.cloudinary.com/dcmcgucbu/image/upload/f_auto,q_auto/v1/icon/ijnninzaazujqoewqeuk"
+    },
+    {
+        title: "PHP",
+        subtext: "Lenguaje de programación para web",
+        icon: "https://res.cloudinary.com/dcmcgucbu/image/upload/f_auto,q_auto/v1/icon/ebjss3fc1bunhqz7xcw9"
+    },
+    {
+        title: "Laravel",
+        subtext: "Marco de desarrollo para aplicaciones web",
+        icon: "https://res.cloudinary.com/dcmcgucbu/image/upload/f_auto,q_auto/v1/icon/pdydk81ccttqqjx3hhtv"
+    },
+    {
+        title: "Docker",
+        subtext: "Plataforma de contenedores para ejecutar aplicaciones",
+        icon: "https://res.cloudinary.com/dcmcgucbu/image/upload/f_auto,q_auto/v1/icon/lbwyx4ujgakmxivqyrtv"
+    }
+
+]
